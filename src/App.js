f86fbed694923c77e082278a63fe7fc27a3f6839
Rahul Mahotra <rahulmahotra707@gmail.com>
2022-03-24 21:54:19 +0530

    first push - IMBD Clone

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+import Movies from './Components/Movies'
+import Favourites from './Components/Favourites'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+
+import './App.css';
+
+function App() {
+  return (
+    
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={[<Banner/>,<Movies/>]} />
+          <Route path="/favourites" element={[<Favourites/>]} />
+        </Routes>
+
+      </Router>
+  );
+}
+
+export default App;
